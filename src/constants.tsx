1446cf9763010bcc41d264e45d54eff6ac9e07f5
Kabir Khalil <81596192+khalilkm01@users.noreply.github.com>
2024-03-15 03:49:37 +0000

    Added API Links

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,3 +1,4 @@
-export const baseApiUrl: string = process.env.API || "http://localhost:4003";
+// export const baseApiUrl: string = process.env.API || "http://localhost:4003"; // For Local Dev
+export const baseApiUrl: string = "159.65.28.185";
 export const apiRoot: string = `${baseApiUrl}/api`;
 export const healthCheckUrl: string = `${baseApiUrl}/health`;
