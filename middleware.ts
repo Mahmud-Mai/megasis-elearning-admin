0f40108d7f012d459cfde460c097058a7af7d33c
Kabir Khalil <81596192+khalilkm01@users.noreply.github.com>
2024-04-06 05:58:20 +0100

    CORS Error Fix

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,51 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-export function middleware(request: NextRequest) {
-  const currentUserToken = localStorage.getItem('bearer-token');
+import { NextRequest, NextResponse } from 'next/server'
 
-  console.log(currentUserToken)
-  if (currentUserToken) {
-    return NextResponse.redirect(new URL('/dashboard', request.url))
-  }
-  return NextResponse.redirect(new URL('/', request.url))
+const allowedOrigins = ['https://acme.com', 'https://my-app.org']
+
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 }
- 
+
+export function middleware(request: NextRequest) {
+
+
+  const currentUserToken = localStorage.getItem('bearer-token')?? ''
+
+  const origin = request.headers.get('origin') ?? '*'
+  const isAllowedOrigin = allowedOrigins.includes(origin)
+
+  // Handle preflighted requests
+  const isPreflight = request.method === 'OPTIONS'
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+      ...corsOptions,
+    }
+    return NextResponse.json({}, { headers: preflightHeaders })
+  }
+
+  // Handle simple requests
+  const response = NextResponse.next()
+
+
+  response.headers.set('Access-Control-Allow-Origin', origin)
+
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value)
+  })
+
+  return response
+
+  // if (currentUserToken) {
+  // return NextResponse.redirect(new URL('/dashboard', request.url))
+  // }
+  // return NextResponse.redirect(new URL('/', request.url))
+}
+
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  // matcher: '/',
 }
+
