8eefdc33bf0f5f08b23fc637b01722930c4e2957
Mahmud-Mai <mahmudmai360@gmail.com>
2024-04-08 11:31:06 +0100

    Style Sidebar Menu: hover state

--- src/components/menuTile/menuTile.tsx
+++ src/components/menuTile/menuTile.tsx
@@ -1,20 +1,23 @@
-import Link from 'next/link'
-import { usePathname } from 'next/navigation';
-import React from 'react'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 export interface MenuNavLinkObj {
-    name: string;
-    address: string;
-    icon: React.ReactNode;
+  name: string;
+  address: string;
+  icon: React.ReactNode;
 }
 
-
-
 export default function MenuTile({ name, address, icon }: MenuNavLinkObj) {
-    const pathname = usePathname()
-    return (
-        <Link href={address} className={`flex items-center px-8 py-3 text-white hover:bg-[#509CDB] group ${pathname === address && "bg-[#509CDB]"}`}>
-            {icon}
-            <span className="ms-3">{name}</span>
-        </Link>
-    )
+  const pathname = usePathname();
+  return (
+    <Link
+      href={address}
+      className={`flex items-center px-8 py-3 text-white hover:opacity-80 group ${
+        pathname === address && "bg-[#509CDB] shadow-lg"
+      }`}
+    >
+      {icon}
+      <span className="ms-3">{name}</span>
+    </Link>
+  );
 }
