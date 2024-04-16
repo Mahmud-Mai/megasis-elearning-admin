9482b9a68cbcbb2a346d9f598ce5f555a5f8d3ca
Mahmud-Mai <mahmudmai360@gmail.com>
2024-04-16 16:50:50 +0100

    Vercel deploy: remove react-bootsrap from /dashboard/profile

--- src/app/dashboard/profile/changepassword/page.tsx
+++ src/app/dashboard/profile/changepassword/page.tsx
@@ -1,26 +1,24 @@
-"use client"
-import { useRouter } from "next/navigation"
-import { useState } from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { updatePassword } from "@/core/services/login-service";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Alert } from "react-bootstrap";
+  CardTitle
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function ChangePassword() {
   const router = useRouter();
 
-  const [currentPassword, setCurrentPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
   const handleSubmit = () => {
     const loginId = localStorage.getItem("userId") || "";
@@ -32,36 +30,51 @@ export default function ChangePassword() {
         localStorage.removeItem("bearer-token");
         localStorage.removeItem("userId");
         router.replace("/");
-      }).catch((error) => {
-        alert("Failed to update password!")
-        console.log("Operation failed")
       })
-  }
+      .catch((error) => {
+        alert("Failed to update password!");
+        console.log("Operation failed");
+      });
+  };
 
   return (
     <section className="flex  align-middle justify-center">
       <Card className="w-[350px] m-5">
         <CardHeader>
-          <CardTitle className="text-center uppercase font-bold">Change Password</CardTitle>
+          <CardTitle className="text-center uppercase font-bold">
+            Change Password
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <form>
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="name">Current Password</Label>
-                <Input value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} id="currentPassword" placeholder="Your Current Password" type="password" />
+                <Input
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                  id="currentPassword"
+                  placeholder="Your Current Password"
+                  type="password"
+                />
               </div>
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="name">New Password</Label>
-                <Input value={newPassword} onChange={(e) => setNewPassword(e.target.value)} id="newPassword" placeholder="New Current Password" type="password" />
+                <Input
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  id="newPassword"
+                  placeholder="New Current Password"
+                  type="password"
+                />
               </div>
             </div>
           </form>
         </CardContent>
         <CardFooter className="flex justify-evenly">
-          <Button onClick={handleSubmit} >Submit</Button>
+          <Button onClick={handleSubmit}>Submit</Button>
         </CardFooter>
       </Card>
     </section>
-  )
+  );
 }
