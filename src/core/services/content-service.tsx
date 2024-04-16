962357e9ece143d6e199b414dbfe72091af5bcb6
Mahmud-Mai <mahmudmai360@gmail.com>
2024-04-16 18:12:12 +0100

    Vercel deploy: fix TS props

--- src/core/services/content-service.tsx
+++ src/core/services/content-service.tsx
@@ -32,212 +32,236 @@ export const getMediaByChapterIdUrl: string = `${apiRoot}/getMediaByChapterId`;
 export const getAllMediaByMediaTypeUrl: string = `${apiRoot}/getAllMediaByMediaType`;
 export const mediaNameAvailableUrl: string = `${apiRoot}/mediaNameAvailable`;
 export const uploadDocumentUrl: string = `${apiRoot}/uploadDocument`;
-export const getSingedUploadUrlUrl: string = `${apiRoot}/uploadVideo`; // TODO update this for the correct url 
+export const getSingedUploadUrlUrl: string = `${apiRoot}/uploadVideo`; // TODO update this for the correct url
 
-export async function createCourse(req: CreateCourseRequest): Promise<CourseDTO> {
-    return await fetch(createCourseUrl, {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(req)
-    }).then(res => res.json());
+export async function createCourse(
+  req: CreateCourseRequest
+): Promise<CourseDTO> {
+  return await fetch(createCourseUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req)
+  }).then((res) => res.json());
 }
 
-export async function updateCourse(req: UpdateCourseRequest): Promise<CourseDTO> {
-    return await fetch(updateCourseUrl, {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(req)
-    }).then(res => res.json());
+export async function updateCourse(
+  req: UpdateCourseRequest
+): Promise<CourseDTO> {
+  return await fetch(updateCourseUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req)
+  }).then((res) => res.json());
 }
 
 export async function deleteCourse(courseId: string): Promise<void> {
-    return await fetch(`deleteCourseUrl/${courseId}`, {
-        method: "DELETE",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-            'Content-Type': 'application/json'
-        }
-    }).then(res => res.json());
+  return await fetch(`deleteCourseUrl/${courseId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`,
+      "Content-Type": "application/json"
+    }
+  }).then((res) => res.json());
 }
 
 export async function getCourse(courseId: string): Promise<CourseDTO> {
-    return await fetch(`${getCourseUrl}/${courseId}`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+  return await fetch(`${getCourseUrl}/${courseId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
 
 export async function getCourses(): Promise<CourseDTO[]> {
-    return await fetch(getCoursesUrl, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+  return await fetch(getCoursesUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
 
 export async function getChapter(chapterId: string): Promise<ChapterDTO> {
-    return await fetch(`${getChapterUrl}/${chapterId}`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+  return await fetch(`${getChapterUrl}/${chapterId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
 
-export async function getChaptersByCourseId(courseId: string): Promise<ChapterDTO[]> {
-    return await fetch(`${getChaptersByCourseIdUrl}/${courseId}`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+export async function getChaptersByCourseId(
+  courseId: string
+): Promise<ChapterDTO[]> {
+  return await fetch(`${getChaptersByCourseIdUrl}/${courseId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
 
-export async function createChapter(req: CreateChapterRequest): Promise<ChapterDTO> {
-    return await fetch(createChapterUrl, {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(req)
-    }).then(res => res.json());
+export async function createChapter(
+  req: CreateChapterRequest
+): Promise<ChapterDTO> {
+  return await fetch(createChapterUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req)
+  }).then((res) => res.json());
 }
 
-export async function updateChapter(req: UpdateChapterRequest): Promise<ChapterDTO> {
-    return await fetch(updateChapterUrl, {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(req)
-    }).then(res => res.json());
+export async function updateChapter(
+  req: UpdateChapterRequest
+): Promise<ChapterDTO> {
+  return await fetch(updateChapterUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req)
+  }).then((res) => res.json());
 }
 
 export async function deleteChapter(chapterId: string): Promise<void> {
-    return await fetch(`${deleteChapterUrl}/${chapterId}`, {
-        method: "DELETE",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-            'Content-Type': 'application/json'
-        }
-    }).then(res => res.json());
+  return await fetch(`${deleteChapterUrl}/${chapterId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`,
+      "Content-Type": "application/json"
+    }
+  }).then((res) => res.json());
 }
 
-export async function deleteChaptersByCourseId(courseId: string): Promise<void> {
-    return await fetch(`${deleteChaptersByCourseIdUrl}/${courseId}`, {
-        method: "DELETE",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+export async function deleteChaptersByCourseId(
+  courseId: string
+): Promise<void> {
+  return await fetch(`${deleteChaptersByCourseIdUrl}/${courseId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
 
 export async function createMedia(req: CreateMediaRequest): Promise<MediaDTO> {
-    return await fetch(createMediaUrl, {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(req)
-    }).then(res => res.json());
+  return await fetch(createMediaUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req)
+  }).then((res) => res.json());
 }
 
 export async function updateMedia(req: UpdateMediaRequest): Promise<MediaDTO> {
-    return await fetch(updateMediaUrl, {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(req)
-    }).then(res => res.json());
+  return await fetch(updateMediaUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req)
+  }).then((res) => res.json());
 }
 
 export async function deleteMedia(mediaId: string): Promise<void> {
-    return await fetch(`${deleteMediaUrl}/${mediaId}`, {
-        method: "DELETE",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+  return await fetch(`${deleteMediaUrl}/${mediaId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
 
 export async function deleteMediaByChapterId(chapterId: string): Promise<void> {
-    return await fetch(`${deleteMediaByChapterIdUrl}/${chapterId}`, {
-        method: "DELETE",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+  return await fetch(`${deleteMediaByChapterIdUrl}/${chapterId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
 
 export async function getMedia(mediaId: string): Promise<MediaDTO> {
-    return await fetch(`${getMediaUrl}/${mediaId}`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+  return await fetch(`${getMediaUrl}/${mediaId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
 
-export async function getMediaByChapterId(chapterId: string): Promise<MediaDTO[]> {
-    return await fetch(`${getMediaByChapterIdUrl}/${chapterId}`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+export async function getMediaByChapterId(
+  chapterId: string
+): Promise<MediaDTO[]> {
+  return await fetch(`${getMediaByChapterIdUrl}/${chapterId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
 
-export async function getAllMediaByMediaType(mediaType: string): Promise<MediaDTO[]> {
-    return await fetch(`${getAllMediaByMediaTypeUrl}/${mediaType}`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+export async function getAllMediaByMediaType(
+  mediaType: string
+): Promise<MediaDTO[]> {
+  return await fetch(`${getAllMediaByMediaTypeUrl}/${mediaType}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
 
-export async function mediaNameAvailable(name: string, chapterId: string): Promise<boolean> {
-    return await fetch(`${mediaNameAvailableUrl}/${chapterId}/${name}`, {
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        }
-    }).then(res => res.json());
+export async function mediaNameAvailable(
+  name: string,
+  chapterId: string
+): Promise<boolean> {
+  return await fetch(`${mediaNameAvailableUrl}/${chapterId}/${name}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    }
+  }).then((res) => res.json());
 }
-
 
 //TODO: Implement Multi part form upload later on!
-export async function getSignedUploadUrl(req: GetSignedUploadUrlRequest): Promise<SignedUploadUrlDTO> {
-    return await fetch(getSingedUploadUrlUrl, {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${localStorage.getItem("bearer-token")}`,
-        },
-        body: JSON.stringify(req)
-    }).then(res => res.json());
+export async function getSignedUploadUrl(
+  req: GetSignedUploadUrlRequest
+): Promise<SignedUploadUrlDTO> {
+  return await fetch(getSingedUploadUrlUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("bearer-token")}`
+    },
+    body: JSON.stringify(req)
+  }).then((res) => res.json());
 }
 
-export async function uploadFile(signedUrl: string, file: File, onUploadProgress: (prog: ProgressEvent) => void): Promise<void> {
-    return await axios.put(signedUrl, {
-        method: 'PUT',
-        body: file,
-        headers: {
-            'Content-Type': file.type,
-        },
-        onUploadProgress,
-    });
+export async function uploadFile(
+  signedUrl: string,
+  file: File,
+  onUploadProgress: (prog: ProgressEvent | number) => void
+): Promise<void> {
+  return await axios.put(signedUrl, {
+    method: "PUT",
+    body: file,
+    headers: {
+      "Content-Type": file.type
+    },
+    onUploadProgress
+  });
 }
